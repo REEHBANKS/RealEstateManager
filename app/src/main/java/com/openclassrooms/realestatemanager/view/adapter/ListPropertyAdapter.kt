8f9818227c8af28eaf7b70982b58d318d6c499ce
imac27 <r.sainfort@gmail.com>
2023-12-07 01:26:31 +0100

    - Implemented XML layout for property list. - Added photo handling and main picture management. - Conducted tests, everything works as expected.

--- app/src/main/java/com/openclassrooms/realestatemanager/view/adapter/ListPropertyAdapter.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/view/adapter/ListPropertyAdapter.kt
@@ -9,11 +9,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.openclassrooms.realestatemanager.R
 import com.openclassrooms.realestatemanager.view.activity.DetailActivity
 import com.openclassrooms.realestatemanager.databinding.FragmentListRealEstatePropertyFragmentItemBinding
 import com.openclassrooms.realestatemanager.data.models.PropertyModels
 import com.openclassrooms.realestatemanager.data.models.PropertyWithMainPicture
+import java.text.NumberFormat
+import java.util.Locale
 
 
 class PropertyAdapter(private val context: Context) : RecyclerView.Adapter<PropertyAdapter.PropertyViewHolder>() {
@@ -36,7 +39,27 @@ class PropertyAdapter(private val context: Context) : RecyclerView.Adapter<Prope
 
     override fun onBindViewHolder(holder: PropertyViewHolder, position: Int) {
         val property = properties[position]
-        holder.binding.textViewPropertyName.text = property.property.type
+        holder.binding.textViewPropertyType.text = property.property.type
+        holder.binding.textViewPropertyNeighborhood.text = property.property.neighborhood
+
+        val format = NumberFormat.getCurrencyInstance(Locale.FRANCE)
+        val priceText = format.format(property.property.price)
+        holder.binding.textViewPropertyPrice.text = priceText
+
+        // Check if there is an image URL available for the property
+        if (property.mainPicture?.uri.isNullOrEmpty()) {
+            // No image URL is available, so set a default image
+            holder.binding.imageViewProperty.setImageResource(R.drawable.picture_intro)
+        } else {
+            // There is an image URL - use your preferred image loading library to load the image
+            // For example, if you are using Glide:
+            Glide.with(context)
+                .load(property.mainPicture?.uri)
+                .into(holder.binding.imageViewProperty)
+        }
+
+
+
     }
 
     override fun getItemCount() = properties.size
