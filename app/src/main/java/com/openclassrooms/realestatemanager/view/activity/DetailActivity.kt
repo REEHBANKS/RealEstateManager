2f402386f6b6f16b314bc41605f53ac7c45b7a55
imac27 <r.sainfort@gmail.com>
2023-12-13 23:14:55 +0100

    updating dependencies

--- app/src/main/java/com/openclassrooms/realestatemanager/view/activity/DetailActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/view/activity/DetailActivity.kt
@@ -53,7 +53,8 @@ class DetailActivity : AppCompatActivity() {
         val mapImageView = findViewById<ImageView>(R.id.mapContainer)
 
         // Construction de l'URL pour Google Maps Static API
-        val staticMapUrl = "https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$longitude&zoom=16&size=600x300&markers=color:red%7C$latitude,$longitude&key=$apiKey"
+        val staticMapUrl = "https://maps.googleapis.com/maps/api/staticmap?center=" +
+                "$latitude,$longitude&zoom=16&size=600x300&markers=color:red%7C$latitude,$longitude&key=$apiKey"
 
         Glide.with(this)
             .load(staticMapUrl)
