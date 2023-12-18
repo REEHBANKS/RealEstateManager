d396d6d0a1be06600d5508cc3c4288fa9dba95e4
reneldev <reneldigital@gmail.com>
2023-12-18 14:56:37 +0100

    Fixed location request setup and updates
    - Updated the setupLocationRequest method to use the non-deprecated approach for creating a LocationRequest instance.
    - Ensured the location updates are requested using the correct method parameters.
    - Cleaned up the code for requesting location permissions and handling the location update callback.

--- app/src/main/java/com/openclassrooms/realestatemanager/view/activity/DetailActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/view/activity/DetailActivity.kt
@@ -36,7 +36,7 @@ class DetailActivity : AppCompatActivity() {
         observePicturesViewModel()
         observeAgentViewModel()
 
-        val apiKey = BuildConfig.API_KEY
+        val apiKey = BuildConfig.RR_KEY
 
 
 
--- app/src/main/java/com/openclassrooms/realestatemanager/view/fragment/MapPropertyFragment.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/view/fragment/MapPropertyFragment.kt
@@ -2,17 +2,23 @@ package com.openclassrooms.realestatemanager.view.fragment
 
 import android.content.pm.PackageManager
 import android.location.Location
+import android.os.Build
 import android.os.Bundle
+import android.os.Looper
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -24,6 +30,7 @@ import com.openclassrooms.realestatemanager.R
 import com.openclassrooms.realestatemanager.databinding.FragmentMapRealEstatePropertyBinding
 import dagger.hilt.android.AndroidEntryPoint
 
+
 @AndroidEntryPoint
 class MapPropertyFragment : Fragment(), OnMapReadyCallback {
     private var _binding: FragmentMapRealEstatePropertyBinding? = null
@@ -31,60 +38,98 @@ class MapPropertyFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var map: GoogleMap
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: LocationCallback
+
     private val LOCATION_PERMISSION_REQUEST_CODE = 1
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         Log.d("MapDebug", "onCreateView called")
         _binding = FragmentMapRealEstatePropertyBinding.inflate(inflater, container, false)
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         Log.d("MapDebug", "onViewCreated called")
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
 
-        // Ici, nous utilisons le View Binding pour trouver le fragment
-        val mapFragment = childFragmentManager.findFragmentById(binding.map.id) as SupportMapFragment
+        val mapFragment =
+            childFragmentManager.findFragmentById(binding.map.id) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        setupLocationRequest()
+        setupLocationCallback()
+    }
+
+    private fun setupLocationRequest() {
+        locationRequest = LocationRequest.create().apply {
+            interval = 10000
+            fastestInterval = 5000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+    }
+
+
+    private fun setupLocationCallback() {
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                @Suppress("UNUSED_EXPRESSION")
+                locationResult
+                for (location in locationResult.locations) {
+                    updateMapLocation(location)
+                }
+            }
+        }
+    }
+
+    private fun updateMapLocation(location: Location) {
+        Log.d("MapDebug", "Location updated: ${location.latitude}, ${location.longitude}")
+        val currentLatLng = LatLng(location.latitude, location.longitude)
+        map.addMarker(MarkerOptions().position(currentLatLng).title("Ma Position"))
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 17f))
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         Log.d("MapDebug", "onMapReady called")
         map = googleMap
-        getLastKnownLocation()
 
         val uiSettings = map.uiSettings
         uiSettings.isZoomControlsEnabled = true
 
-
+        startLocationUpdates()
     }
 
-    private fun getLastKnownLocation() {
-        Log.d("MapDebug", "getLastKnownLocation called")
+    private fun startLocationUpdates() {
         if (ContextCompat.checkSelfPermission(
                 requireContext(),
                 android.Manifest.permission.ACCESS_FINE_LOCATION
             ) == PackageManager.PERMISSION_GRANTED
         ) {
-            fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
-                Log.d("MapDebug", "Location retrieved: ${location?.latitude}, ${location?.longitude}")
-                location?.let {
-                    val currentLatLng = LatLng(it.latitude, it.longitude)
-                    map.addMarker(MarkerOptions().position(currentLatLng).title("Ma Position"))
-                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 17f))
-                }
-            }.addOnFailureListener { exception ->
-                Log.e("MapDebug", "Location retrieval failed", exception)
-            }
+            fusedLocationClient.requestLocationUpdates(
+                locationRequest,
+                locationCallback,
+                Looper.getMainLooper()
+            )
         } else {
             requestLocationPermission()
         }
     }
+
+    private fun stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        stopLocationUpdates()
+    }
+
 
     private fun requestLocationPermission() {
         if (ActivityCompat.shouldShowRequestPermissionRationale(
@@ -131,7 +176,7 @@ class MapPropertyFragment : Fragment(), OnMapReadyCallback {
     ) {
         if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                getLastKnownLocation()
+                startLocationUpdates()
             } else {
 
                 // Show a toast message if permission is denied
@@ -143,6 +188,7 @@ class MapPropertyFragment : Fragment(), OnMapReadyCallback {
             }
         }
     }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
