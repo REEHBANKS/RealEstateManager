5bbb2bee4e41c555d3f923c13ea862f2c071c56e
imac27 <r.sainfort@gmail.com>
2024-01-09 09:31:19 +0100

    "Enhanced SearchActivity with Multi-Select Filter Chips in Compose"
    - Added multi-select filter chips for property types and proximity criteria.
    - Managed multiple selection states with a Set<String> for dynamic filtering.
    - Refined TextField UI for min/max values, adjusting text alignment and box dimensions.
    - Eliminated default TextField outlines for a cleaner appearance and maximized space.
    - Resolved UI issues ensuring text fields are appropriately sized and positioned.

--- app/src/main/java/com/openclassrooms/realestatemanager/view/activity/SearchActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/view/activity/SearchActivity.kt
@@ -36,10 +36,12 @@ import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -49,6 +51,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -69,6 +72,7 @@ class MyComposeActivity : ComponentActivity() {
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun FilterScreen() {
+    val selectedProximityItems = remember { mutableStateOf(setOf<String>()) }
     Scaffold(
         topBar = {
             TopAppBar(
@@ -122,6 +126,23 @@ fun FilterScreen() {
 
             )
             MinMaxInputFields()
+
+            Divider(modifier = Modifier.padding(top = 0.dp))
+
+            Text(
+                text = "Proximity",
+                style = MaterialTheme.typography.h6.merge(
+                    androidx.compose.ui.text.TextStyle(
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black
+                    )
+                )
+
+            )
+            MultipleFilterChipRow(selectedItems = selectedProximityItems)
+
+            Divider(modifier = Modifier.padding(top = 30.dp))
         }
 
 
@@ -135,7 +156,7 @@ fun FilterChipRow() {
         horizontalArrangement = Arrangement.SpaceBetween,
         modifier = Modifier
             .fillMaxWidth()
-            .padding(top = 15.dp) // Ajouter de l'espace entre le texte "Type" et la Row
+            .padding(top = 15.dp)
     ) {
         FilterChip(text = "Flat", isSelected = true, onSelected = { /* TODO */ })
         Spacer(modifier = Modifier.width(8.dp))
@@ -190,6 +211,13 @@ fun MinMaxInputFields() {
         }
     }
 
+    Box(
+        modifier = Modifier
+            .height(100.dp)
+            .fillMaxWidth(), // Remplit la taille maximale du parent
+        contentAlignment = Alignment.Center // Centre le contenu à l'intérieur du Box
+    ){
+
     Row(
         modifier = Modifier
             .background(Color.White)
@@ -203,19 +231,29 @@ fun MinMaxInputFields() {
                 .width(70.dp) // Largeur fixe pour le chip
                 .height(55.dp) // Hauteur fixe pour le chip
                 .border(1.dp, Color.Blue)
-                .padding(4.dp)
+                .padding(1.dp)
         ) {
             TextField(
                 value = minValue,
-                onValueChange = { newValue ->  // "newValue" est le nom que vous avez choisi pour remplacer "it"
+                onValueChange = { newValue ->
                     if (newValue.all { char -> char.isDigit() }) {
                         minValue = newValue
                         validateValues()
                     }
                 },
-                textStyle = androidx.compose.ui.text.TextStyle(color = Color.Gray),
-                placeholder = { Text(text = "min.") },
-                singleLine = true
+                textStyle = androidx.compose.ui.text.TextStyle(color = Color.Gray,
+                    fontSize = 13.sp),
+                placeholder = { Text(text = "1500", fontSize = 13.sp) },
+                singleLine = true,
+                modifier = Modifier.width(120.dp),
+                colors = TextFieldDefaults.textFieldColors(
+                    backgroundColor = Color.Transparent, // Pour un arrière-plan transparent
+                    unfocusedIndicatorColor = Color.Transparent, // Pas de ligne en dessous quand il n'est pas focus
+                    focusedIndicatorColor = Color.Transparent
+                ),
+                // Supprimez le padding par défaut du TextField pour plus d'espace
+                visualTransformation = VisualTransformation.None,
+                maxLines = 1
             )
 
         }
@@ -225,7 +263,7 @@ fun MinMaxInputFields() {
                 .width(70.dp) // Largeur fixe pour le chip
                 .height(55.dp) // Hauteur fixe pour le chip
                 .border(1.dp, Color.Blue)
-                .padding(4.dp)
+                .padding(1.dp)
         ) {
             TextField(
                 value = maxValue,
@@ -235,15 +273,81 @@ fun MinMaxInputFields() {
                         validateValues()
                     }
                 },
-                textStyle = androidx.compose.ui.text.TextStyle(color = Color.Gray),
-                placeholder = { Text("max.") },
-                singleLine = true
+                textStyle = androidx.compose.ui.text.TextStyle(color = Color.Gray,
+                    fontSize = 13.sp),
+                placeholder = { Text(text = "1500", fontSize = 13.sp) },
+                singleLine = true,
+                modifier = Modifier.width(120.dp),
+                colors = TextFieldDefaults.textFieldColors(
+                    backgroundColor = Color.Transparent, // Pour un arrière-plan transparent
+                    unfocusedIndicatorColor = Color.Transparent, // Pas de ligne en dessous quand il n'est pas focus
+                    focusedIndicatorColor = Color.Transparent
+                ),
+                // Supprimez le padding par défaut du TextField pour plus d'espace
+                visualTransformation = VisualTransformation.None,
+                maxLines = 1
             )
+        }
+    }
+    }
+}
+
+@Composable
+fun MultipleFilterChipRow(selectedItems: MutableState<Set<String>>) {
+    val chipLabels = listOf("schools", "shops", "parks", "station")
+
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 15.dp) // Ajouter de l'espace entre le texte "Proximity" et la Row
+    ) {
+        // Créez un FilterChip pour chaque label
+        chipLabels.forEach { label ->
+            FilterChip(
+                text = label,
+                isSelected = selectedItems.value.contains(label),
+                onSelected = {
+                    // Mise à jour de l'état de sélection
+                    val currentSelection = selectedItems.value.toMutableSet()
+                    if (currentSelection.contains(label)) {
+                        currentSelection.remove(label)
+                    } else {
+                        currentSelection.add(label)
+                    }
+                    selectedItems.value = currentSelection
+                }
+            )
+            Spacer(modifier = Modifier.width(8.dp))
         }
     }
 }
 
+@Composable
+fun FilterChipProximity(text: String, isSelected: Boolean, onSelected: () -> Unit) {
 
+    Surface(
+        modifier = Modifier
+            .width(70.dp) // Largeur fixe pour le chip
+            .height(35.dp) // Hauteur fixe pour le chip
+            .clip(RoundedCornerShape(20)) // Coins arrondis pour un look de chip
+            .clickable(onClick = onSelected), // Rend le chip cliquable
+        color = if (isSelected) Color.Blue else Color.Transparent,
+        border = BorderStroke(1.dp, Color.Blue)
+    ) {
+        Box(contentAlignment = Alignment.Center) {
+            Text(
+                text = text,
+                fontSize = 13.sp,
+                textAlign = TextAlign.Center,
+                color = if (isSelected) Color.White else Color.Gray,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
+
+
+}
 
 
 
